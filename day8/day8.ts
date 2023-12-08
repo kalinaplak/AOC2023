4e12ae4d8d906b76430770a90f1487fafa13364c
Kalina Rząsowska <kalina.rzasowska@comarch.pl>
2023-12-08 11:46:51 +0100

    day8 - part1

---
+++ day8/day8.ts
@@ -0,0 +1,33 @@
+import { Dictionary } from "powerseq/common/types";
+const fs = require("fs");
+
+type Direction = "R" | "L";
+type Instruction = { L: string; R: string };
+
+const input = fs.readFileSync("./day8/input.txt", "utf-8");
+
+function parseInput(input: string) {
+  const [line1, _, ...lines] = input.split("\n");
+   const directionMap: Dictionary<Instruction> = lines.reduce((acc,l) => {
+    acc[l.slice(0, 3)] = { L: l.slice(7, 10), R: l.slice(12, 15) };
+    return acc;
+  }, {});
+  
+  return { instructions: [...line1] as Direction[], directionMap };
+}
+
+export function processInstructions({instructions, directionMap}) {
+  const endCondition = "ZZZ";
+  let currentLocation = "AAA";
+  let steps = 0;
+
+  while (currentLocation !== endCondition) {
+    const instructionIndex = steps % instructions.length;
+    currentLocation = directionMap[currentLocation][instructions[instructionIndex]];
+    steps++;
+  }
+
+  return steps;
+}
+
+console.log(processInstructions(parseInput(input)));
