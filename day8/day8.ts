56aa77804e0ce5c2e2317e2ab1dc2075b158317e
Kalina Rząsowska <kalina.rzasowska@comarch.pl>
2023-12-08 12:16:48 +0100

    day 8 - part2

--- day8/day8.ts
+++ day8/day8.ts
@@ -16,18 +16,35 @@ function parseInput(input: string) {
   return { instructions: [...line1] as Direction[], directionMap };
 }
 
-function processInstructions({instructions, directionMap}, startLocation) {
-  const endCondition = "ZZZ";
+//1.
+function processInstructions({instructions, directionMap}, startLocation, endConditionFn: (loc) => boolean) {
   let currentLocation = startLocation;
   let steps = 0;
-
-  while (currentLocation !== endCondition) {
+  while (!endConditionFn(currentLocation)) {
     const instructionIndex = steps % instructions.length;
     currentLocation = directionMap[currentLocation][instructions[instructionIndex]];
     steps++;
   }
-
   return steps;
 }
 
-console.log(processInstructions(parseInput(input), "AAA"));
+console.log(processInstructions(parseInput(input), "AAA", l => l === "ZZZ"));
+
+//2.
+function findGreatestCommonDivisor(a: number, b: number): number {
+  // Euclidean algorithm to find Greatest Common Divisor
+  return b === 0 ? a : findGreatestCommonDivisor(b, a % b);
+}
+
+function getLeastCommonMultiple(a:number, b:number){
+  const gcd = findGreatestCommonDivisor(a, b);
+  return (a * b) / gcd;
+}
+
+function processGhostInstructions({ instructions, directionMap }) {
+  const starts = Object.keys(directionMap).filter((id) => id[2] === "A");
+  const lengths = starts.map((start) => processInstructions({instructions, directionMap}, start, l => l[2] === "Z"));
+  return lengths.reduce((acc, curr) => getLeastCommonMultiple(curr, acc), 1);
+}
+
+console.log(processGhostInstructions(parseInput(input)));
