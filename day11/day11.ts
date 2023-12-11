5f9f09d66f49ff77a2ed8c869d54a769d5de39a3
Kalina Rząsowska <kalina.rzasowska@comarch.pl>
2023-12-11 20:42:46 +0100

    day11 - part2

--- day11/day11.ts
+++ day11/day11.ts
@@ -47,7 +47,36 @@ function calculateDistancesSum(universe: string[][]){
   )
 }
 
-const universeMap = parseInput(input);
-console.log(calculateDistancesSum(universeMap));
+console.log(calculateDistancesSum(parseInput(input)));
 
+//2. 
+function getIndexesToExpand(universe: string[][]){
+  return  pipe(
+    universe,
+    map((row, index) => ({ row, index })),
+    filter(({ row }) => row.every(item => item === '.')),
+    map(({ index }) => index),
+    toarray()
+  );
+}
+
+function calculateDistancesWithExpandLevel(universe: string[][], expandLevel = 1000000){
+  const expandRows =getIndexesToExpand(universe);
+  const expandColumns = getIndexesToExpand(transpose(universe));
+  const galaxiesPoints = getGalaxiesPoints(universe);
+  const expandRatio = expandLevel -1;
+  return pipe(
+    galaxiesPoints,
+    flatmap((x, i) => galaxiesPoints.slice(i + 1).map(y => [x, y])), //pairs
+    map(([[p1x, p1y],[p2x, p2y]])=> {
+      const distance = Math.abs(p1x - p2x) + Math.abs(p1y - p2y);
+      const expandedRowsInRange = expandRows.filter(x => (p1x < x && x < p2x) || (p2x < x && x < p1x)).length * expandRatio;
+      const expandedColsInRange = expandColumns.filter(y => (p1y < y && y < p2y) || (p2y < y && y < p1y)).length * expandRatio;
+      return distance + expandedRowsInRange + expandedColsInRange;
+    }),
+    sum()
+  )
+}
+
+console.log(calculateDistancesWithExpandLevel(parseInput(input), 1000000));
 
