7541bfc9b72802832c9552a98f4358472b8ed6b5
Kalina Rząsowska <kalina.rzasowska@comarch.pl>
2023-12-19 08:34:00 +0100

    day13 - part2

--- day13/day13.ts
+++ day13/day13.ts
@@ -1,4 +1,4 @@
-import { filter, map, pipe, range, sum, toarray } from "powerseq";
+import { count, filter, map, pipe, range, sum, toarray } from "powerseq";
 
 const fs = require("fs");
 
@@ -20,6 +20,10 @@ function compareRows(row1: string[], row2: string[]){
   return row1.join('') === row2.join('');
 }
 
+function getMarchingCharactersInRow(row1: string[], row2: string[]){
+  return pipe( row1, count((r1v, i)=> r1v === row2[i]));
+}
+
 function getNormalizedRows(matrix: string[][], col: number){
   const [row1, row2] = [matrix.slice(0,col), matrix.slice(col, matrix.length)];
   const p1 = row1.length > row2.length ? row1.slice(-row2.length) : row1;
@@ -27,32 +31,44 @@ function getNormalizedRows(matrix: string[][], col: number){
   return [p1,p2];
 }
 
-function checkMirror(matrix: string[][], col: number){
+function checkMirror(matrix: string[][], col: number, allowUnequal){
   const [r1,r2] = getNormalizedRows(matrix, col);
-  return r1.every((r, i) => compareRows(r, r2[i]))
+
+  if(!allowUnequal){
+    return r1.every((r, i) => compareRows(r, r2[i]))
+  } else{
+    const comparedSum = pipe(r1.map((r, i) => getMarchingCharactersInRow(r, r2[i])), sum());
+    const totalSum = pipe(r1, map(r => r.length), sum())
+    //results with 1 error 
+    return comparedSum === totalSum -1;
+  }
 }
 
-function checkMatrix(matrix: string[][]){
+function checkMatrix(matrix: string[][], allowUnequal){
   return pipe(
     range(0, matrix.length),
-    map(col => checkMirror(matrix, col) ? col : null),
+    map(col => checkMirror(matrix, col, allowUnequal) ? col : null),
     filter (col => !!col),
     sum()
   )
 }
 
-function checkMatrixes(matrixes: string[][][]){
+function checkMatrixes(matrixes: string[][][], allowUnequal = false){
   return pipe(
     matrixes,
     map(m => {
-      const check = checkMatrix(m);
+      const check = checkMatrix(m, allowUnequal);
       const transposed = check === 0;
-      const transposedCheck = transposed ? checkMatrix(transpose(m)) : null;
-      return transposed ? transposedCheck : check * 100;
+      const transposedCheck = transposed ? checkMatrix(transpose(m), allowUnequal) : null;
+      return {check: transposed ? transposedCheck : check, transposed, matrix: m}
     }),
+    map((m:any) => m.transposed ? m.check : m.check * 100),
     sum()
   )
 }
 
 const matrixes = parseInput(input);
+//1.
 console.log(checkMatrixes(matrixes));
+//2.
+console.log(checkMatrixes(matrixes, true));
