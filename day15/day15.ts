cfff975d6e0d53f80f6ef0decdc39681dc171954
Kalina Rząsowska <kalina.rzasowska@comarch.pl>
2023-12-19 13:14:12 +0100

    day15 - part2

--- day15/day15.ts
+++ day15/day15.ts
@@ -1,4 +1,4 @@
-import { map, pipe, sum } from "powerseq";
+import { flatmap, map, pipe, range, sum, toarray } from "powerseq";
 
 const fs = require("fs");
 
@@ -12,6 +12,7 @@ function processLine(line: string[]){
   return line.reduce((acc,curr)=>((acc + curr.charCodeAt(0)) * 17) % 256, 0)
 }
 
+//1.
 function part1(input:string[][]){
   return pipe(
     input,
@@ -22,3 +23,37 @@ function part1(input:string[][]){
 
 const lines = parseInput(input);
 console.log(part1(lines));
+
+//2.
+function calculateBoxes(input: string[][]){
+  const emptyBoxes = pipe(range(0, 255), map(r => new Map<string, number>()), toarray());
+
+  return input.reduce((acc,curr)=>{
+    if(curr[curr.length -1] === '-'){
+      const toDelete = curr.slice(0, curr.length -1);
+      acc[processLine(toDelete)].delete(toDelete.join(''));
+    } else{
+      const [line, focal] = curr.join('').split('=');
+      const processed = processLine(line.split(''));
+      
+      if(!acc[processed]) acc[processed] = new Map<string, number>();
+      acc[processed].set(line, parseInt(focal));
+    }
+    return acc;
+  }, emptyBoxes);
+}
+
+function part2(input: string[][]){
+  return pipe(
+    calculateBoxes(input),
+    flatmap((b, i) => pipe(
+      Array.from(b.values()),
+      map((focal, j) => (i + 1) * (j + 1) * focal),
+      toarray()
+    )),
+    sum()
+  );
+
+}
+
+console.log(part2(lines));
