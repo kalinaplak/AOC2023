abda8e2e61dd9c0e182f4c40a2dfe240c8ea7945
Kalina Rząsowska <kalina.rzasowska@comarch.pl>
2024-01-03 18:34:38 +0100

    day12 - part2 + part1 refactor

--- day12/day12.ts
+++ day12/day12.ts
@@ -1,4 +1,4 @@
-import { every, filter, flatmap, map, pipe, toarray, zip } from "powerseq";
+import { map, pipe, sum, toarray } from "powerseq";
 
 const fs = require("fs");
 
@@ -7,32 +7,6 @@ const input = fs.readFileSync("./day12/input.txt", "utf-8");
 interface SpringRow {
   springRow: string;
   damaged: number[];
-}
-
-function generatePossibleStringRow(inputString: string): string[] {
-  if (!inputString.includes("?")) return [inputString];
-
-  const index: number = inputString.indexOf("?");
-  const withDot: string[] = inputString.split("");
-  const withHash: string[] = inputString.split("");
-
-  withDot[index] = ".";
-  withHash[index] = "#";
-
-  const stringsWithDot: string[] = generatePossibleStringRow(withDot.join(""));
-  const stringsWithHash: string[] = generatePossibleStringRow(
-    withHash.join("")
-  );
-
-  return stringsWithDot.concat(stringsWithHash);
-}
-
-function isCorrectLine(springGroups: string[], damaged: number[]) {
-  if (springGroups.length !== damaged.length) return false;
-  return pipe(
-    zip(springGroups, damaged, (g, d) => [g, d]),
-    every(([spring, num]: any) => spring.length === num)
-  );
 }
 
 function parseInput(input: string): SpringRow[] {
@@ -49,19 +23,66 @@ function parseInput(input: string): SpringRow[] {
 
 const springRows = parseInput(input);
 
-function getPossibleStringRows(springRows: SpringRow[]) {
-  return pipe(
-    springRows,
-    flatmap((springRowData) =>
-      pipe(
-        generatePossibleStringRow(springRowData.springRow),
-        map((r) => r.split(".").filter((g) => !!g)),
-        filter((r) => isCorrectLine(r, springRowData.damaged)),
-        toarray()
-      )
-    ),
-    toarray()
-  ).length;
+function getMapKey(springRow, damaged) {
+  return [springRow, damaged.join(",")].join(" ");
 }
 
-console.log(getPossibleStringRows(springRows));
+function trimDots(springRow: string){
+  return springRow.replace(/^\.+|\.+$/, '');
+}
+
+function getNextDamagedWithoutUnknownGroupLength(springRow: string){
+  return springRow.match(/^#+(?=\.|$)/)?.[0]?.length || 0;
+}
+
+function replaceUnknownChar(char: '#' | '.', sr: string, damaged: number[]): SpringRow{
+  return { springRow: sr.replace('?', char), damaged };
+}
+
+function countPossibleOptions({ springRow, damaged }: SpringRow, cache): number {
+  springRow = trimDots(springRow);
+  if (springRow === '') return damaged.length ? 0 : 1;
+  if (!damaged.length) return springRow.includes('#') ? 0 : 1;
+
+  const key = getMapKey(springRow, damaged);
+  if (cache.has(key)) return cache.get(key);
+
+  let result = 0;
+  const damagedLength = getNextDamagedWithoutUnknownGroupLength(springRow);
+  if (damagedLength) {
+    if (damagedLength === damaged[0]) {
+      result += countPossibleOptions({ springRow: springRow.slice(damaged[0]), damaged: damaged.slice(1)}, cache);
+    }
+  } else if (springRow.includes('?')) {
+    result += countPossibleOptions(replaceUnknownChar('.', springRow, damaged), cache);
+    result += countPossibleOptions(replaceUnknownChar('#', springRow, damaged), cache);
+  }
+  
+  cache.set(key, result);
+  return result;
+}
+
+function part1(input: SpringRow[]) {
+  const cache = new Map<string, number>();
+  return pipe(
+    input,
+    map(sr => countPossibleOptions(sr, cache)),
+    sum()
+  )
+}
+
+function part2(input: SpringRow[]) {
+  const cache = new Map<string, number>();
+  return pipe(
+    input,
+    map(sr => ({
+      springRow: [...Array(5)].fill(sr.springRow).join('?'),
+      damaged: [...Array(5)].fill(sr.damaged).flat()
+    })),
+    map(sr => countPossibleOptions(sr, cache)),
+    sum()
+  )
+}
+
+console.log(part1(springRows));
+console.log(part2(springRows));
