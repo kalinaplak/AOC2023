d2e3c4c4882d7b72543b48b7f30cc0c89d797744
Kalina Rząsowska <kalina.rzasowska@comarch.pl>
2024-01-03 13:42:59 +0100

    day14 part2

--- day14/day14.ts
+++ day14/day14.ts
@@ -1,62 +1,136 @@
-import fs from 'fs';
-import { count, filter, map, pipe, range, sum, toarray } from 'powerseq';
+import fs from "fs";
+import { count, filter, map, pipe, range, sum, toarray } from "powerseq";
 
-const input = fs.readFileSync('./day14/input.txt', 'utf8');
+const input = fs.readFileSync("./day14/input.txt", "utf8");
+type Direction = "up" | "down" | "left" | "right";
 
-function parseInput(input){
-  return input.split("\n").map(l => l.trim().split('')) 
+function parseInput(input) {
+  return input.split("\n").map((l) => l.trim().split(""));
 }
 
-function canMoveUp(matrix, i, j) {
-  return i > 0 && matrix[i - 1][j] === ".";
+function canMove(matrix: string[][], i, j, dir: Direction) {
+  switch (dir) {
+    case "down":
+      return i < matrix.length - 1 && matrix[i + 1][j] === ".";
+    case "up":
+      return i > 0 && matrix[i - 1][j] === ".";
+    case "left":
+      return j > 0 && matrix[i][j - 1] === ".";
+    case "right":
+      return j < matrix[0].length - 1 && matrix[i][j + 1] === ".";
+  }
 }
 
-function moveUp(matrix, i, j) {
-  matrix[i][j] = ".";
-  matrix[i - 1][j] = "O";
+function move(matrix: string[][], i, j, dir: Direction) {
+  switch (dir) {
+    case "down":
+      matrix[i][j] = ".";
+      matrix[i + 1][j] = "O";
+      return;
+    case "up":
+      matrix[i][j] = ".";
+      matrix[i - 1][j] = "O";
+      return;
+    case "left":
+      matrix[i][j] = ".";
+      matrix[i][j - 1] = "O";
+      return;
+    case "right":
+      matrix[i][j] = ".";
+      matrix[i][j + 1] = "O";
+      return;
+  }
 }
 
-function moveRock(matrix, i, j) {
-  while (canMoveUp(matrix, i, j)) {
-    moveUp(matrix, i, j);
-    i -= 1;
+function performRockMove(matrix, i, j, dir: Direction) {
+  while (canMove(matrix, i, j, dir)) {
+    move(matrix, i, j, dir);
+    switch (dir) {
+      case "up":
+        i -= 1;
+        break;
+      case "down":
+        i += 1;
+        break;
+      case "left":
+        j -= 1;
+        break;
+      case "right":
+        j += 1;
+        break;
+    }
   }
 }
 
 function findRockPositions(matrix) {
-  return matrix.flatMap((row, i) => pipe(
-    row,
-    map((cell, j) => ({ cell, i, j })),
-    filter(({ cell }) => cell === 'O'),
-    toarray()
-  ));
+  return matrix.flatMap((row, i) =>
+    pipe(
+      row,
+      map((cell, j) => ({ cell, i, j })),
+      filter(({ cell }) => cell === "O"),
+      toarray()
+    )
+  );
 }
 
-function moveAllRocks(matrix){
-  const rocks = findRockPositions(matrix);
-  rocks.forEach(r =>{
-    moveRock(matrix, r.i, r.j);
-  })
+function moveAllRocks(matrix, dir: Direction) {
+  let rocksToMove = findRockPositions(matrix);
+  while (rocksToMove.length > 0) {
+    // const rocks = findRockPositions(matrix);
+    rocksToMove.forEach((r) => {
+      performRockMove(matrix, r.i, r.j, dir);
+    });
+    rocksToMove = findRockPositions(matrix).filter((r) =>
+      canMove(matrix, r.i, r.j, dir)
+    );
+  }
 }
 
 function countRocksInRow(i, matrix) {
   return pipe(
     matrix[i],
-    count(c => c === 'O')
-  )
+    count((c) => c === "O")
+  );
 }
 
 function countTotalLoad(matrix) {
   return pipe(
     range(0, matrix.length),
     sum((i) => (matrix.length - i) * countRocksInRow(i, matrix))
-  )
+  );
 }
 
-function part1(matrix){
-  moveAllRocks(matrix);
+function matrixToString(matrix: string[][]) {
+  return matrix.map((m) => m.join("")).join("\n");
+}
+
+function part1(matrix) {
+  moveAllRocks(matrix, "up");
   return countTotalLoad(matrix);
 }
 
+function part2(matrix) {
+  const cycles = 1000000000;
+  let cycleStateIndex = -1;
+  let index = 0;
+  const states = [matrixToString(matrix)];
+  while (cycleStateIndex === -1) {
+    index += 1;
+    moveAllRocks(matrix, "up");
+    moveAllRocks(matrix, "left");
+    moveAllRocks(matrix, "down");
+    moveAllRocks(matrix, "right");
+    const newState = matrixToString(matrix);
+    cycleStateIndex = states.findIndex((s) => s === newState);
+    if (cycleStateIndex === -1) states.push(newState);
+  }
+
+  const cycleLength = index - cycleStateIndex;
+  const indexToCheck =
+    cycleStateIndex + ((cycles - cycleStateIndex) % cycleLength);
+  return countTotalLoad(parseInput(states[indexToCheck]));
+}
+
 const matrix = parseInput(input);
-console.log(part1(matrix));
+console.log(part1(JSON.parse(JSON.stringify(matrix))));
+console.log(part2(JSON.parse(JSON.stringify(matrix))));
